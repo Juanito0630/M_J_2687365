892aa6bd35565792429586c99d3c0423506c01ce
Mauricio1606 <micmbarreto@soy.sena.edu.co>
2023-10-20 08:49:04 -0500

    oli

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "M_J_2687365"
+include(":app")
